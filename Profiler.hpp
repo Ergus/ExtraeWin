5f0c421b8badaf7a9cf29fa1f45772631e10562b
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2023-07-01 05:37:20 +0200

    Use int types for events and values and long for tid
    char types are too small and the struct has the same size any way,
    size_t is not standard sizes, so difficult to parse with numpy.

--- Profiler.hpp
+++ Profiler.hpp
@@ -11,37 +11,37 @@ namespace profiler {
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 
-    #include <processthreadsapi.h>
-    #include <winbase.h>
-    #include <windows.h>
+	#include <processthreadsapi.h>
+	#include <winbase.h>
+	#include <windows.h>
 
-    int getNumberOfCores() {
-        SYSTEM_INFO sysinfo;
-        GetSystemInfo(&sysinfo);
-        return sysinfo.dwNumberOfProcessors;
-    }
+	int getNumberOfCores() {
+		SYSTEM_INFO sysinfo;
+		GetSystemInfo(&sysinfo);
+		return sysinfo.dwNumberOfProcessors;
+	}
 
-    unsigned int getCPUId()
-    {
-        PROCESSOR_NUMBER procNumber;
-        GetCurrentProcessorNumberEx(&procNumber);
-        return procNumber.Group * 64 + procNumber.Number;
-    }
+	unsigned int getCPUId()
+	{
+		PROCESSOR_NUMBER procNumber;
+		GetCurrentProcessorNumberEx(&procNumber);
+		return procNumber.Group * 64 + procNumber.Number;
+	}
 
 #else
 
-    #include <unistd.h>
+	#include <unistd.h>
 
-    int getNumberOfCores() {
-        return sysconf(_SC_NPROCESSORS_ONLN);
-    }
+	int getNumberOfCores() {
+		return sysconf(_SC_NPROCESSORS_ONLN);
+	}
 
-    unsigned int getCPUId()
-    {
-        const int cpu = sched_getcpu();
-        assert(cpu >= 0);
-        return cpu;
-    }
+	unsigned int getCPUId()
+	{
+		const int cpu = sched_getcpu();
+		assert(cpu >= 0);
+		return cpu;
+	}
 
 #endif
 
@@ -51,44 +51,44 @@ namespace profiler {
 	}
 
 
-    /**
-       Buffer class to store the events.
+	/**
+	   Buffer class to store the events.
 
-       This class will always have a file associated to it to flush all the data when needed.  There
-       will be 1 Buffer/Core in order to make the tracing events registration completely lock free.
-    */
+	   This class will always have a file associated to it to flush all the data when needed.  There
+	   will be 1 Buffer/Core in order to make the tracing events registration completely lock free.
+	*/
 	template <size_t BUFFERSIZE>
-    class Buffer {
+	class Buffer {
 
-        /**
-           Event struct that will be reported (and saved into the traces files in binary format)
+		/**
+		   Event struct that will be reported (and saved into the traces files in binary format)
 
-           I reserve all the negative values for internal events. And positive values for
-           user/application events.
-           Every starting event needs and end event. But I cannot ensure that they will be in the
-           same cpu (due to thread migration), but they will in the same thread. So, I also save the
-           threadId with every event in order to make a right reconstruction latter.
-         */
-        struct EventEntry {
-            const char _id;
-            const unsigned char _value;
-			const size_t _tid;
+		   I reserve all the negative values for internal events. And positive values for
+		   user/application events.
+		   Every starting event needs and end event. But I cannot ensure that they will be in the
+		   same cpu (due to thread migration), but they will in the same thread. So, I also save the
+		   threadId with every event in order to make a right reconstruction latter.
+		 */
+		struct EventEntry {
+			const int _id;
+			const unsigned int _value;
+			const unsigned long _tid;
 			const unsigned long _time;
 
-            EventEntry(char id, unsigned char value, size_t tid)
-                : _id(id),
-                  _value(value),
+			EventEntry(int id, unsigned int value, unsigned long tid)
+				: _id(id),
+				  _value(value),
 				  _tid(tid),
-                  _time(getMicrosecondsTime())
-            {
-            }
-        };
+				  _time(getMicrosecondsTime())
+			{
+			}
+		};
 
-		static constexpr size_t _maxEntries = ( BUFFERSIZE + sizeof(EventEntry) - 1 ) / sizeof(EventEntry);  //< Maximum size for the buffers ~ 1Mb >/
+		static constexpr size_t _maxEntries = ( BUFFERSIZE + sizeof(EventEntry) - 1 ) / sizeof(EventEntry);	 //< Maximum size for the buffers ~ 1Mb >/
 
 		struct TraceHeader {
 			unsigned int _cpuID;
-			size_t _totalFlushed;
+			unsigned long _totalFlushed;
 
 			TraceHeader(TraceHeader&& other)
 				: _cpuID(other._cpuID),
@@ -104,30 +104,30 @@ namespace profiler {
 			{}
 		} _header;
 
-        const std::string _fileName;  //< Name of the binary file with trace information
-        std::ofstream _file;          //< fstream with file open; every write will be appended and binary. >/
-        std::vector<EventEntry> _entries; //< Buffer with entries; it will be flushed when the number of entries reach _maxEntries;
+		const std::string _fileName;  //< Name of the binary file with trace information
+		std::ofstream _file;		  //< fstream with file open; every write will be appended and binary. >/
+		std::vector<EventEntry> _entries; //< Buffer with entries; it will be flushed when the number of entries reach _maxEntries;
 
-        void flushBuffer()
-        {
-            if (_entries.empty())
-                return;
+		void flushBuffer()
+		{
+			if (_entries.empty())
+				return;
 
-            _file.write(reinterpret_cast<char *>(_entries.data()), _entries.size() * sizeof(EventEntry));
-            _header._totalFlushed += _entries.size();
-            _file.clear();
-        }
+			_file.write(reinterpret_cast<char *>(_entries.data()), _entries.size() * sizeof(EventEntry));
+			_header._totalFlushed += _entries.size();
+			_file.clear();
+		}
 
-      public:
+	  public:
 
-        explicit Buffer(size_t cpuIdx)
-            : _header(),
+		explicit Buffer(size_t cpuIdx)
+			: _header(),
 			  _fileName("Trace_" + std::to_string(cpuIdx) + ".bin"),
-              _file(_fileName.c_str(), std::ios::out | std::ios::binary),
-              _entries()
-        {
-            _entries.reserve(_maxEntries);
-            assert(_file.is_open());
+			  _file(_fileName.c_str(), std::ios::out | std::ios::binary),
+			  _entries()
+		{
+			_entries.reserve(_maxEntries);
+			assert(_file.is_open());
 
 			// Reserve size for the header
 			_file.write(reinterpret_cast<char *>(&_header), sizeof(TraceHeader));
@@ -137,7 +137,7 @@ namespace profiler {
 			// of static members (the singleton) to register them.
 			emplace(-1, 1, 0);
 
-        }
+		}
 
 		Buffer(Buffer&& other)
 			: _header(std::move(other._header)),
@@ -152,41 +152,41 @@ namespace profiler {
 
 		   Declared outside in order to call singleton functions.
 		*/
-        ~Buffer();
+		~Buffer();
 
-        /**
-           Add (report) a new event.
+		/**
+		   Add (report) a new event.
 
-           This adds a new entry in the buffer with a value and id. when id is zero it means that it
-           is the end of the event
-        */
-        void emplace(char id, unsigned char value, size_t tid)
-        {
-            _entries.emplace_back(id, value, tid);
+		   This adds a new entry in the buffer with a value and id. when id is zero it means that it
+		   is the end of the event
+		*/
+		void emplace(int id, unsigned int value, unsigned long tid)
+		{
+			_entries.emplace_back(id, value, tid);
 
-            if (_entries.size() >= _maxEntries)
-                flushBuffer();
+			if (_entries.size() >= _maxEntries)
+				flushBuffer();
 
-            assert(_entries.size() < _maxEntries);
-        }
-    };
+			assert(_entries.size() < _maxEntries);
+		}
+	};
 
-	template<size_t BUFFERSIZE = (1 << 20)>  //< Maximum size for the buffers ~ 1Mb >/
-    class ProfilerGuard {
+	template<size_t BUFFERSIZE = (1 << 20)>	 //< Maximum size for the buffers ~ 1Mb >/
+	class ProfilerGuard {
 
-        class BufferSet {
+		class BufferSet {
 
 		  public:
 			using buffer_t = Buffer<BUFFERSIZE>;
 
-            BufferSet() :
+			BufferSet() :
 				_size(getNumberOfCores()),
 				_file("Trace.txt", std::ios::out)
-            {
+			{
 				assert(_file.is_open());
-                for (size_t i = 0; i < _size; ++i)
-                    _profileBuffers.emplace_back(i);
-            }
+				for (size_t i = 0; i < _size; ++i)
+					_profileBuffers.emplace_back(i);
+			}
 
 			void AddReport(const std::string& filename)
 			{
@@ -195,50 +195,50 @@ namespace profiler {
 				_file << filename << std::endl;
 			}
 
-            inline buffer_t &operator[](size_t idx)
-            {
-                assert(idx < _size);
-                return _profileBuffers[idx];
-            }
+			inline buffer_t &operator[](size_t idx)
+			{
+				assert(idx < _size);
+				return _profileBuffers[idx];
+			}
 
 		  private:
 			const size_t _size;
 
-			std::mutex _fileMutex;   //< mutex needed to write in the global file >/
+			std::mutex _fileMutex;	 //< mutex needed to write in the global file >/
 			std::ofstream _file;
 
 			// This AFTER _file to destroy in right order (RAII)
 			std::vector<buffer_t> _profileBuffers;
 
-        };
+		};
 
-        const char _id;
-        const size_t _tid;
+		const int _id;
+		const size_t _tid;
 
-        static void registerNewEvent(char id, unsigned char value, size_t tid)
-        {
-            unsigned int cpu = getCPUId();
-            _singleton[cpu].emplace(id, value, tid);
-        }
+		static void registerNewEvent(int id, unsigned int value, unsigned long tid)
+		{
+			unsigned int cpu = getCPUId();
+			_singleton[cpu].emplace(id, value, tid);
+		}
 
-      public:
+	  public:
 
 		static BufferSet _singleton;
 
-        ProfilerGuard(char id, char value)
-            : _id(id),
-              _tid(std::hash<std::thread::id>()(std::this_thread::get_id()))
-        {
-            assert(value != 0);
-            registerNewEvent(_id, value, _tid);
-        }
+		ProfilerGuard(int id, unsigned int value)
+			: _id(id),
+			  _tid(std::hash<std::thread::id>()(std::this_thread::get_id()))
+		{
+			assert(value != 0);
+			registerNewEvent(_id, value, _tid);
+		}
 
-        ~ProfilerGuard()
-        {
-            registerNewEvent(_id, 0, _tid);
-        }
+		~ProfilerGuard()
+		{
+			registerNewEvent(_id, 0, _tid);
+		}
 
-    };
+	};
 
 	template <size_t T>
 	typename ProfilerGuard<T>::BufferSet ProfilerGuard<T>::_singleton;
