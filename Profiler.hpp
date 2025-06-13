71c80e7b882d2ce85d52deea886c9efcc8b182fa
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2025-06-13 23:16:43 +0000

    Return to my "single header" policy

--- Profiler.hpp
+++ Profiler.hpp
@@ -644,6 +644,75 @@ namespace profiler {
 
 	}; // ProfilerGuard
 
+#define INSTRUMENT_EVENT(ID, VALUE) \
+	profiler::Global<>::traceMemory = false;				            \
+	profiler::Global<>::getInfoThread().eventsBuffer.emplaceEvent(ID, VALUE); \
+	profiler::Global<>::traceMemory = true;				            \
+
+
+	/** Intrumented mutex
+
+		This is an intrumented mutex wrapper intended to register information
+		about lock contentions.
+		*/
+	class mutex {
+	public:
+		constexpr mutex() noexcept
+		{
+			unsigned char expected = 0;
+			// Globals: 0 = no initialized, 1 = initialization in progress, 2 = already initialized
+			if (registered.compare_exchange_strong(expected, 1)) {
+				bool tmp = profiler::Global<>::traceMemory;
+				profiler::Global<>::traceMemory = false;
+
+				instrument_mutex_id = profiler::registerName("Mutex", "", 0, 0, 0);
+				[[maybe_unused]] uint16_t wait_value = profiler::registerName("Waiting", "", 0, instrument_mutex_id, instrument_waiting);
+				assert(instrument_waiting == wait_value);
+
+				profiler::Global<>::traceMemory = tmp;
+				registered.store(2); // Perform this always at the end if the initialization.
+			}
+
+			// block until the init members are set. This is very unlikely needed, but may happen.
+			while (registered.load() != 2);
+
+			_id = _counter.fetch_add(1, std::memory_order_relaxed);
+		}
+
+		mutex(const mutex &) = delete;
+
+		void lock()
+		{
+			INSTRUMENT_EVENT(instrument_mutex_id, instrument_waiting)
+			_lock.lock();
+			INSTRUMENT_EVENT(instrument_mutex_id, _id)
+		}
+
+		void unlock()
+		{
+			_lock.unlock();
+			INSTRUMENT_EVENT(instrument_mutex_id, 0)
+		}
+
+		bool try_lock()
+		{
+			const bool locked = _lock.try_lock();
+			if (locked)
+				INSTRUMENT_EVENT(instrument_mutex_id, _id)
+			return locked;
+		}
+
+	private:
+		static inline std::atomic<unsigned char> registered = 0;
+		static inline uint16_t instrument_mutex_id;
+		static constexpr uint16_t instrument_waiting = 1;	  /// Reserve the event 1 to blocked by a mutex
+		static inline std::atomic<unsigned int> _counter = instrument_waiting + 1; /// The lock counter starts after it
+
+
+		unsigned int _id;
+		std::mutex _lock;
+	};
+
 	// ==================================================
 	// Outline function definitions.
 	// ==================================================
@@ -834,10 +903,6 @@ inline void operator delete(void* ptr, size_t sz) noexcept
 	profiler::ProfilerGuard<> __guard(__profiler_function_id, CAT(__profiler_function_,__LINE__));		\
 	profiler::Global<>::traceMemory = true;
 
-#define INSTRUMENT_EVENT(ID, VALUE) \
-	profiler::Global<>::traceMemory = false;				            \
-	profiler::Global<>::getInfoThread().eventsBuffer.emplaceEvent(ID, VALUE); \
-	profiler::Global<>::traceMemory = true;				            \
 
 /** Main macro to instrument functions subsections.
 
@@ -860,8 +925,13 @@ inline void operator delete(void* ptr, size_t sz) noexcept
 
 #else // PROFILER_ENABLED
 
+#define INSTRUMENT_EVENT(...)
 #define INSTRUMENT_SCOPE(...)
 #define INSTRUMENT_FUNCTION(...)
 #define INSTRUMENT_FUNCTION_UPDATE(...)
+
+namespace profiler {
+    using mutex = std::mutex;
+}
 
 #endif // PROFILER_ENABLED
